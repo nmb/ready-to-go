42bc96003872fe169fd0bdda6e012bdfd5404cc8
Mikael Borg <mikael.borg@nbis.se>
2017-12-12 15:23:43 +0100

    Install to /opt, as /usr might be read-only.

--- script/install-docker-compose.sh
+++ script/install-docker-compose.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 echo "Installing docker-compose:"
-sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo mkdir -p /opt/bin/
+sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /opt/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 echo "docker-compose installed"
