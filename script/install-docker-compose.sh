2ea24a3d0b0a5b8b9df495e224a540549e348f13
Mikael Borg <mikael.borg@bils.se>
2017-12-13 11:24:49 +0100

    Fix bug w/ installation location.

--- script/install-docker-compose.sh
+++ script/install-docker-compose.sh
@@ -3,5 +3,5 @@
 echo "Installing docker-compose:"
 sudo mkdir -p /opt/bin/
 sudo curl -L https://github.com/docker/compose/releases/download/1.17.0/docker-compose-`uname -s`-`uname -m` -o /opt/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
+sudo chmod +x /opt/bin/docker-compose
 echo "docker-compose installed"
